c4ff2cfcaef38a450fde3901aa8e797ca1d6e1e7
Peter Jones <pjsoftware@petesplace.id.au>
2019-12-04 08:49:54 +1000

    The grok moment has occurred. I understand how interface{} works for these deeply nested data types; now I just need to dig down to the relevant data so I can match these items with the incoming images--and add detection of irrelevant JSON files. Then just throw all that into its own OO package and we're done!

--- _reference/scan_json.go
+++ _reference/scan_json.go
@@ -4,26 +4,86 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"os"
 )
 
 func main() {
-
-	jsonFile, err := os.Open("1575245198.json")
-
+	data, err := readJSON("1575245198.json")
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
-	defer jsonFile.Close()
-
-	byteValue, _ := ioutil.ReadAll(jsonFile)
 
 	var result map[string]interface{}
-	json.Unmarshal([]byte(byteValue), &result)
+	json.Unmarshal(data, &result)
 
 	batchRSP := result["batchrsp"].(map[string]interface{})
+	items := batchRSP["items"].([]interface{})
 
-	for key, value := range batchRSP {
-		fmt.Println(key, value.(string))
+	for idx, obj := range items {
+		fmt.Printf("====================\nItem %d:\n", idx)
+		itemObj := obj.(map[string]interface{})
+		itemStr := itemObj["item"]
+		itemBytes := []byte(itemStr.(string))
+
+		var itemMap map[string]interface{}
+		json.Unmarshal(itemBytes, &itemMap)
+
+		adObj := itemMap["ad"].(map[string]interface{})
+		fmt.Printf("'ad' subMap:\n\n")
+		for key, item := range adObj {
+			fmt.Printf("%s: %v\n\n", key, item)
+		}
 	}
 }
+
+func readJSON(fileName string) ([]byte, error) {
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	if !json.Valid(data) {
+		return nil, fmt.Errorf("Specified file does not contain valid JSON")
+	}
+
+	return data, nil
+}
+
+// func checkErr(err error) {
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
+
+// func readFile(filePath string) []byte {
+// 	data, err := ioutil.ReadFile(filePath)
+// 	checkErr(err)
+// 	return data
+// }
+
+// func main() {
+// 	path := "1575245198.json"
+// 	data := readFile(path)
+
+// 	var f interface{}
+
+// 	err := json.Unmarshal(data, &f)
+// 	checkErr(err)
+
+// 	m := f.(map[string]interface{})
+
+// 	for k, v := range m {
+// 		switch vv := v.(type) {
+// 		case string:
+// 			fmt.Println(k, "is string", vv)
+// 		case int:
+// 			fmt.Println(k, "is int", vv)
+// 		case []interface{}:
+// 			fmt.Println(k, "is an array:")
+// 			for i, u := range vv {
+// 				fmt.Println(i, u)
+// 			}
+// 		default:
+// 			fmt.Println(k, "is of a type I don't know how to handle")
+// 		}
+// 	}
+// }
