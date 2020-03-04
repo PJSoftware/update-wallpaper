44784c90161e36bb169d98f31ae1293585341908
Peter Jones <pjsoftware@petesplace.id.au>
2020-03-04 12:02:30 +1000

    Used new ValueOptional() so that testing-only SpotlightContentFolder will no longer be logged as missing in regular non-testing usage.

--- spotlight/config.go
+++ spotlight/config.go
@@ -29,11 +29,13 @@ func (s *Config) Init(exePath string) {
 	s.Width = sectWallpaper.Value("ImageWidth").AsInt(1920)
 	s.Height = sectWallpaper.Value("ImageHeight").AsInt(1080)
 	s.TargetPath = sectWallpaper.Value("DestinationFolder").AsString(`C:\Wallpaper`, false)
-	contentRoot := sectWallpaper.Value("SpotlightContentFolder").AsString(GetPaths().ContentRoot(), false)
-	GetPaths().SetContentRoot(contentRoot)
-	s.SourcePath = GetPaths().Assets()
 
 	sectPrefix := s.iniFile.Section("Prefix")
 	s.Prefix = sectPrefix.Value("Prefix").AsString("ZZZ_Unsorted-", false)
 	s.SmartPrefix = sectPrefix.Value("SmartPrefix").AsBool(true)
+
+	// SpotlightContentFolder should only be specified in testing
+	contentRoot := sectWallpaper.ValueOptional("SpotlightContentFolder").AsString(GetPaths().ContentRoot(), false)
+	GetPaths().SetContentRoot(contentRoot)
+	s.SourcePath = GetPaths().Assets()
 }
