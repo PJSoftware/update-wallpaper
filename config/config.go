f9b653936c02c25286fa20116e7a95e43aac7e3e
Peter Jones <pjsoftware@petesplace.id.au>
2020-05-19 03:20:41 +1000

    remove code to handle Archive section of ini; no longer used

--- config/config.go
+++ config/config.go
@@ -12,8 +12,6 @@ type Config struct {
 	Width, Height int
 	TargetPath    string
 	SourcePath    string
-	Archive       string
-	DupHandler    string
 	iniFile       ini.File
 }
 
@@ -30,10 +28,6 @@ func (s *Config) Init(exePath string) {
 	s.Width = sectWallpaper.Value("ImageWidth").AsInt(1920)
 	s.Height = sectWallpaper.Value("ImageHeight").AsInt(1080)
 	s.TargetPath = sectWallpaper.Value("DestinationFolder").AsString(`C:\Wallpaper`, false)
-
-	sectArchive := s.iniFile.Section("Archive")
-	s.Archive = sectArchive.Value("Archive").AsString(`_Archive`, false)
-	s.DupHandler = sectArchive.Value("Method").AsString(`Delete`, false)
 
 	// SpotlightContentFolder should only be specified in testing
 	crv := sectWallpaper.ValueOptional("SpotlightContentFolder")
