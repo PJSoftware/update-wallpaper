e96174b3113c7ed233199bb0c44e9d9e2dd1532d
Peter Jones <pjsoftware@petesplace.id.au>
2020-05-19 03:17:33 +1000

    remove DeleteDuplicates; no longer required

--- publish.sh
+++ publish.sh
@@ -5,10 +5,4 @@ go build
 ls -l *.exe
 cd ../..
 
-cd cmd/DeleteDuplicates
-go build
-ls -l *.exe
-cd ../..
-
 cp cmd/UpdateSpotlight/UpdateSpotlight.exe /c/Wallpaper
-cp cmd/DeleteDuplicates/DeleteDuplicates.exe /c/Wallpaper
