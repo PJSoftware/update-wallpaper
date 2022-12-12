114412339522c462d9f9f8aa59a10204432e88ed
Peter Jones <pjsoftware@petesplace.id.au>
2022-12-13 00:13:34 +1000

    bump to v1.4

--- pkg/splashscreen/splashscreen.go
+++ pkg/splashscreen/splashscreen.go
@@ -5,6 +5,6 @@ import "fmt"
 // Show displays our splash "screen" with version & copyright info
 func Show(cmd string) {
 	fmt.Printf("Windows Wallpaper Toolset v%s\n", version)
-	fmt.Printf("%s: Copyright © 2020 by PJSoftware\n", cmd)
+	fmt.Printf("%s: Copyright © 2022 by PJSoftware\n", cmd)
 	fmt.Println()
 }
